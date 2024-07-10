8f4b4cf7321a96105f5f04c8626a5474419bba89
Rgit915 <rora.goitom@gmail.com>
2024-07-10 16:11:27 -0400

    Fix User model initialization typo in database schema

--- models/user.js
+++ models/user.js
@@ -23,6 +23,6 @@ const UserSchema = new Schema({
 // If a model named "User" does not exist in the "models" object, the "model" function from Mongoose is called to create a new model
 // The newly created model is then assigned to the "User" variable.
 
-const User = models.User || model("User, UserSchema");
+const User = models.User || model("User", UserSchema);
 
 export default User;
