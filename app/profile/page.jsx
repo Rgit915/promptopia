47a9bd4089ab5f2f034305c34144f4a2714cfbc7
Rgit915 <rora.goitom@gmail.com>
2024-07-18 18:26:30 -0400

    Add MyProfile component with user posts fetch

---
+++ app/profile/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {useState, useEffect} from 'react';
+import { useSession } from 'next-auth/react';
+
+import Profile from '@components/Profile';
+
+const MyProfile = () => {
+const { data: session } = useSession();
+
+const [posts, setPosts] = useState([]);
+
+  // Fetch user's posts from API on component mount
+useEffect(() =>{
+
+  const fetchPosts = async () => {
+    const response = await fetch(`/api/users/$
+      {session?.user.id}/posts`);
+    const data = await response.json();
+
+    setPosts(data);
+  }
+
+ if(session?.user.id) fetchPosts();
+}, []);
+  return (
+    <Profile
+     name="My"
+     desc="Welcome to your personalized profile page"
+     data={posts}
+    />
+  )
+}
+
+export default MyProfile
