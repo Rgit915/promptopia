8e3386d3ca316136974f8df41c25927224cb1bcb
Rgit915 <rora.goitom@gmail.com>
2024-08-10 18:28:50 -0400

    Add handleEdit, handleDelete, and handleCopy functions

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -2,21 +2,26 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
 
-const PromptCard = ({ post, handleTagClick}) => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data:session } = useSession();
+  const pathName = usePathname();
+
   const [copied, setCopied] = useState("");
 
   // Function to handle copying text to clipboard
-  const handleCopy = () =>{
-      setCopied(post.prompt);
-      navigator.clipboard.writeText(post.prompt);
-      setTimeout(() => setCopied(""), 3000); // reset the state after 3 seconds
-
-  }
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(false), 3000); // reset the state after 3 seconds
+  };
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start  items-center gap-3 cursor-pointer">
+        <div className="flex-1 flex justify-start  items-center gap-3 cursor-pointer"
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
@@ -46,8 +51,29 @@ const PromptCard = ({ post, handleTagClick}) => {
         </div>
       </div>
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p className="font-inter text-sm blue_gradient cursor-pointer "
-        onClick={()=>{ handleTagClick && handleTagClick(post.tag)}}>{post.tag}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer "
+        onClick={() => {
+          handleTagClick && handleTagClick(post.tag);
+        }}
+      >
+        {post.tag}
+      </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p className="font-inter text-sm green_gradient cursor-pointer"
+          onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p className="font-inter text-sm orange_gradient cursor-pointer"
+          onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
