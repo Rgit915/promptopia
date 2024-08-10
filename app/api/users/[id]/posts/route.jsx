d6205cdeb290e01f51f0e557a7edb3e8b353c1e0
Rgit915 <rora.goitom@gmail.com>
2024-08-10 18:47:40 -0400

    Add GET handler to fetch prompts by creator ID with population

---
+++ app/api/users/[id]/posts/route.jsx
@@ -0,0 +1,24 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+// GET request handler to fetch all prompts
+export const GET = async (request, { params }) => {
+  try {
+    // Connect to the database
+    await connectToDB();
+
+    // Fetch all prompts from the database and populate the 'creator' field
+    const prompts = await Prompt.find({
+      creator: params.id
+    }).populate('creator');
+
+    // Return a success response with the fetched prompts
+    return new Response(JSON.stringify(prompts), {
+      status: 200
+    });
+  } catch (error) {
+    // Return an error response if fetching prompts fails
+    return new Response("Failed to fetch all prompts", { status: 500 });
+
+  }
+};
